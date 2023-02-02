9049e7471d7835a6e96e0a611984eef497696853
kees <122539445+kaleeitjevandebuurt@users.noreply.github.com>
2023-02-02 14:51:29 +0100

    Create kaas.JS

---
+++ kaas.JS
@@ -0,0 +1,23 @@
+function knopje() {
+document.getElementById("eerste_foto").style.display='block';
+document.getElementById("tweede_foto").style.display='none';
+document.getElementById("derde_foto").style.display='none';
+document.getElementById("vierde_foto").style.display='none';
+}
+
+function knopje() {
+document.getElementById("eerste_foto").style.display='none';
+document.getElementById("tweede_foto").style.display='block';
+document.getElementById("derde_foto").style.display='none';
+document.getElementById("vierde_foto").style.display='none';
+}
+
+function knopje() {
+document.getElementById("eerste_foto").style.display='none';
+document.getElementById("tweede_foto").style.display='none';
+document.getElementById("derde_foto").style.display='block';
+document.getElementById("vierde_foto").style.display='none';
+}
+
+
+
